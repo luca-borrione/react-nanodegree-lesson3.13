14fc60584fd663a6fe94cfda67fef833d2f7492b
Luca Borrione <luca@email.com>
2018-10-19 22:45:33 +0100

    possible solution

--- src/App.js
+++ src/App.js
@@ -3,21 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
-        <div className="container">
-          <input type="text" placeholder="Say Something" />
-          <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
-        </div>
-      </div>
-    );
-  }
+
+	state = {
+		text: ''
+	};
+
+	updateText = event => {
+		this.setState({
+			text: event.target.value
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">ReactND - Coding Practice</h1>
+				</header>
+				<div className="container">
+					<input
+						type="text"
+						placeholder="Say Something"
+						onChange={this.updateText}
+					/>
+					{this.state.text.length > 0 && (
+						<div>
+							<p className="echo">Echo:</p>
+							<p>{this.state.text}</p>
+						</div>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
